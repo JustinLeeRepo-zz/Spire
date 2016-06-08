6c70fd91d8c513a1b4c4eebc93bea0b4962661cc
Justin Lee <justinj.lee23@gmail.com>
2016-06-08 15:46:38 -0700

    Root View and Data fetch
    App delegate sets root view to TableViewController
    TableViewController makes async request to endpoint

--- Spire/AppDelegate.m
+++ Spire/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "TableViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,11 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
 	// Override point for customization after application launch.
+	self.window.backgroundColor = [UIColor whiteColor];
+	self.window.rootViewController = [[TableViewController alloc] init];
+	[self.window makeKeyAndVisible];
 	return YES;
 }
 
--- Spire/TableViewController.m
+++ Spire/TableViewController.m
@@ -6,19 +6,40 @@
 //  Copyright © 2016 Justin Lee. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "TableViewController.h"
+#import <AFNetworking.h>
 
-@interface ViewController ()
+@interface TableViewController ()
 
 @end
 
-@implementation ViewController
+@implementation TableViewController
 
 #pragma mark - View Lifecycle
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	
+	self.view.backgroundColor = [UIColor redColor];
+	[self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
+	
+//	NSDate *currentDate = [[NSDate alloc] init];
+	int secsUtc1970 = [[NSDate date] timeIntervalSince1970];
+	NSString *baseURL = @"http://spire-challenge.herokuapp.com/";
+	
+	NSString *url = [NSString stringWithFormat:@"%@%@/%d", baseURL, @"streaks",secsUtc1970];
+	
+	AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+	[manager GET:url parameters:nil progress:nil success:^(NSURLSessionTask *task, NSArray *responseObject){
+				NSLog(@"HTTP REQUEST SUCCESS %@", responseObject);
+//		dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^(void){
+//			[self parseData:(NSDictionary *)responseObject];
+//		});
+	} failure:^(NSURLSessionTask *operation, NSError *error){
+		NSLog(@"HTTP GET REQUEST FAILED %@", error);
+		return;
+	}];
 }
 
 #pragma mark - Memory Management
