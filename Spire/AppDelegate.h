fef5836bf0d7b0498cec56c4bf9a79cf53e273eb
Justin Lee <justinj.lee23@gmail.com>
2016-06-08 14:44:53 -0700

    Initial Commit

---
+++ Spire/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Spire
+//
+//  Created by Justin Lee on 6/8/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Spire/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Spire
+//
+//  Created by Justin Lee on 6/8/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
